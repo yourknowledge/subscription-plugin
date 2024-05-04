80983637ae5602e1b2bb0bf438f997f551ef6b98
kuouu <kuouu.tw@gmail.com>
2024-05-04 23:34:51 +0800

    register student to course

--- subscription/main.php
+++ subscription/main.php
@@ -16,20 +16,18 @@ if (!function_exists('subscription_period')) {
 if (!function_exists('subscription_payment')) {
     function subscription_payment( $data ) {
         // get request form data
-        $form_data = file_get_contents('php://input');
+        $form_data_str = file_get_contents('php://input');
 
-        // send request to https://notify-api.line.me/api/notify
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://notify-api.line.me/api/notify');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'message='.urlencode($form_data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Authorization: Bearer z3MZ3FUt64VbsvkRav88DUhlUBAN9j1b91JK2gFAzNP',
-            'Content-Type: application/x-www-form-urlencoded'
-        ));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
+        parse_str($form_data_str, $form_data);
+
+        // register user to TutorLMS course
+        $wordpress_post = array(
+            'post_author' => $form_data['CustomField1'],
+            'post_parent' => 8, // TODO: replace with actual course id
+            'post_status' => 'completed',
+            'post_type' => 'tutor_enrolled',
+        );
+        wp_insert_post($wordpress_post);
 
         return "1|OK";
     }
--- test/route.sh
+++ test/route.sh
@@ -1,6 +1,6 @@
 curl http://localhost:8080/wp-json/subscription/test
 curl -X POST http://localhost:8080/wp-json/subscription/order/123
-curl -X POST http://localhost:8080/wp-json/subscription/payment -d "CustomField1=&CustomField2=&CustomField3=&CustomField4=&MerchantID=2000132
+curl -X POST http://localhost:8080/wp-json/subscription/payment -d "CustomField1=1&CustomField2=&CustomField3=&CustomField4=&MerchantID=2000132
 &MerchantTradeNo=Test1510056539&PaymentDate=2017/11/02 16:22:18
 &PaymentType=Credit_CreditCard&PaymentTypeChargeFee=1&RtnCode=1&RtnMsg=交易成功&SimulatePaid=0&StoreID=&TradeAmt=100&TradeDate=2017/11/07 20:08:59&TradeNo=17110720085960236789
 &CheckMacValue=9139AF2AC5D0F9EBC5F3CD44064F666AAA62F0B202B95B341CC25E080EA4FC6E"
