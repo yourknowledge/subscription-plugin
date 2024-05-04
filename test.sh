2d45533117ac558da3c15dccc7bc86b4630feb28
kuouu <kuouu.tw@gmail.com>
2024-05-04 13:39:35 +0800

    add test script

---
+++ test.sh
@@ -0,0 +1,3 @@
+curl http://localhost:8080/wp-json/subscription/test
+curl -X POST http://localhost:8080/wp-json/subscription/order/123
+curl -X POST http://localhost:8080/wp-json/subscription/period/123
