a24a4f7e566f6e954c6a544e0c1c4675d8f9ee22
kuouu <kuouu.tw@gmail.com>
2024-05-07 01:44:12 +0800

    refactor subscription functions

--- subscription/main.php
+++ subscription/main.php
@@ -1,34 +1,29 @@
 <?php
-if (!function_exists('subscription_order')) {
-    function subscription_order( $data ) {
-        $user_id = $data['user_id'];
-        return 'subscription order: '.$user_id;
-    }
+
+function subscription_order( $data ) {
+    $user_id = $data['user_id'];
+    return 'subscription order: '.$user_id;
 }
 
-if (!function_exists('subscription_period')) {
-    function subscription_period( $data ) {
-        $user_id = $data['user_id'];
-        return 'subscription period:'.$user_id;
-    }
+function subscription_period( $data ) {
+    $user_id = $data['user_id'];
+    return 'subscription period:'.$user_id;
 }
 
-if (!function_exists('subscription_payment')) {
-    function subscription_payment( $data ) {
-        // get request form data
-        $form_data_str = file_get_contents('php://input');
+function subscription_payment( $data ) {
+    // get request form data
+    $form_data_str = file_get_contents('php://input');
 
-        parse_str($form_data_str, $form_data);
+    parse_str($form_data_str, $form_data);
 
-        // register user to TutorLMS course
-        $wordpress_post = array(
-            'post_author' => $form_data['CustomField1'],
-            'post_parent' => 8, // TODO: replace with actual course id
-            'post_status' => 'completed',
-            'post_type' => 'tutor_enrolled',
-        );
-        wp_insert_post($wordpress_post);
+    // register user to TutorLMS course
+    $wordpress_post = array(
+        'post_author' => $form_data['CustomField1'],
+        'post_parent' => 9, // TODO: replace with actual course id
+        'post_status' => 'completed',
+        'post_type' => 'tutor_enrolled',
+    );
+    wp_insert_post($wordpress_post);
 
-        return "1|OK";
-    }
+    return "1|OK";
 }
